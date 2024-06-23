409dc501ad7539ab6bc9d780d5f17d546ae6de3f
kmazaflix <110471285+kmazaflix@users.noreply.github.com>
2024-06-23 18:16:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/kmazaflix/URL-Shortener-V2.1 /URL-Shortener-V2.1
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /URL-Shortener-V2.1
 fi
 cd /URL-Shortener-V2
 pip3 install -U -r requirements.txt
